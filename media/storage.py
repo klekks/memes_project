d65fd414a32c8ac020e4d30526dc6481263acd12
Ilya Petrov <klekks@yandex.ru>
2024-07-26 03:08:08 +0300

    Removing secret token leak

--- media/storage.py
+++ media/storage.py
@@ -1,9 +1,10 @@
 from fastapi_storages import S3Storage
+import os
 
 
 class PublicAssetS3Storage(S3Storage):
     AWS_ACCESS_KEY_ID = "501c33c75b0a4afea0146b7e5d64a74e"
-    AWS_SECRET_ACCESS_KEY = "106ea05c6c9a4e2ba67eff294fba35f9"
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_KEY")
     AWS_S3_BUCKET_NAME = "memes-bucket"
     AWS_S3_ENDPOINT_URL = "s3.storage.selcloud.ru"
     AWS_DEFAULT_ACL = "public-read"
